62eb00607bd8c76dce500290e301238f1f53a3ec
Dyvak Yurii <dyvakyurii@gmail.com>
2018-10-10 23:18:29 +0300

    Add super java8 FunctionalInterface Validation.java and FunctionalInterface transactions/Transaction.java

--- src/main/java/model/services/service/UserServiceImpl.java
+++ src/main/java/model/services/service/UserServiceImpl.java
@@ -7,6 +7,8 @@ import model.entities.Product;
 import model.entities.User;
 import model.services.UserService;
 import model.services.exception.ServiceException;
+import model.services.transactions.TransactionHandler;
+import model.services.transactions.TransactionHandlerImpl;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,10 +19,13 @@ import static model.services.exception.ServiceException.USER_ALREADY_EXISTS;
 
 public class UserServiceImpl implements UserService {
 
+    private TransactionHandler transactionHandler = TransactionHandlerImpl.getInstance();
+
+
     private DaoFactory daoFactory = DaoFactory.getInstance();
 
     public void setDaoFactory(DaoFactory daoFactory) {
-        this.daoFactory=daoFactory;
+        this.daoFactory = daoFactory;
     }
 
     private static class Holder {
@@ -32,9 +37,9 @@ public class UserServiceImpl implements UserService {
     }
 
     public Optional<User> login(String email, String password) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             return Optional.ofNullable(userDao.getUserByEmail(email)
                     .filter(user -> (user.calcPasswordHash(password)).equals(user.getPasswordHash()))
                     .orElseThrow(() -> new ServiceException(USER_ALREADY_EXISTS)));
@@ -42,51 +47,51 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<User> getAll() {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             return userDao.findAll();
         }
     }
 
     public List<User> getAllUsersWithOrders() {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             return userDao.findAllUsersWithOrders();
         }
     }
 
     public Optional<User> getByEmail(String email) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             return userDao.getUserByEmail(email);
         }
     }
 
     public Optional<User> getById(int id) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             return userDao.findById(id);
         }
     }
 
-    public Map<User,Map<Order,Map<OrderProduct,Product>>> getUserMap(List<User> userList) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+    public Map<User, Map<Order, Map<OrderProduct, Product>>> getUserMap(List<User> userList) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            OrderProductDao orderProductDao=daoFactory.createOrderProductDao(connection);
-            UserOrderDao userOrderDao=daoFactory.createUserOrderDao(connection);
-            Map<User,Map<Order,Map<OrderProduct,Product>>> userMap = new HashMap<>();
-            for(User user : userList) {
-                List<Order> ordersList=userOrderDao.findAllOrdersForUser(user.getId());
-                Map<Order, Map<OrderProduct, Product>> orderMap=new HashMap<>();
+            OrderProductDao orderProductDao = daoFactory.createOrderProductDao(connection);
+            UserOrderDao userOrderDao = daoFactory.createUserOrderDao(connection);
+            Map<User, Map<Order, Map<OrderProduct, Product>>> userMap = new HashMap<>();
+            for (User user : userList) {
+                List<Order> ordersList = userOrderDao.findAllOrdersForUser(user.getId());
+                Map<Order, Map<OrderProduct, Product>> orderMap = new HashMap<>();
                 for (Order order : ordersList) {
-                    List<OrderProduct> orderProductList=orderProductDao.findOrderProductsByOrderId(order.getId());
-                    Map<OrderProduct, Product> orderProductMap=new HashMap<>();
+                    List<OrderProduct> orderProductList = orderProductDao.findOrderProductsByOrderId(order.getId());
+                    Map<OrderProduct, Product> orderProductMap = new HashMap<>();
                     for (OrderProduct orderProduct : orderProductList) {
-                        Optional<Product> optionalProduct=orderProductDao.findProductByOrderProductId(orderProduct.getId());
+                        Optional<Product> optionalProduct = orderProductDao.findProductByOrderProductId(orderProduct.getId());
                         if (optionalProduct.isPresent()) {
                             Product product = optionalProduct.get();
                             orderProductMap.put(orderProduct, product);
@@ -101,19 +106,32 @@ public class UserServiceImpl implements UserService {
     }
 
     public void create(User user) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
-            connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
-            checkIfUserAlreadyExists(user.getEmail(), userDao);
-            userDao.create(user);
-            connection.commitTransaction();
-        }
+        transactionHandler.runInTransaction(connection -> {
+            transactionHandler
+                    .createUserDao()
+                    .create(user);
+        });
     }
 
+
+//
+//        try(
+//    DaoConnection connection = daoFactory.getConnection())
+//
+//    {
+//        connection.beginTransaction();
+//        UserDao userDao = daoFactory.createUserDao(connection);
+//        checkIfUserAlreadyExists(user.getEmail(), userDao);
+//        userDao.create(user);
+//        connection.commitTransaction();
+//    }
+
+}
+
     public void update(User user) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             user.setPasswordHash(userDao.getPasswordForUser(user));
             userDao.update(user, user.getId());
             connection.commitTransaction();
@@ -121,16 +139,16 @@ public class UserServiceImpl implements UserService {
     }
 
     public void delete(int id) {
-        try(DaoConnection connection = daoFactory.getConnection()) {
+        try (DaoConnection connection = daoFactory.getConnection()) {
             connection.beginTransaction();
-            UserDao userDao=daoFactory.createUserDao(connection);
+            UserDao userDao = daoFactory.createUserDao(connection);
             userDao.delete(id);
             connection.commitTransaction();
         }
     }
 
-    private void checkIfUserAlreadyExists(String email, UserDao userDao){
-        if(userDao.getUserByEmail(email).isPresent()){
+    private void checkIfUserAlreadyExists(String email, UserDao userDao) {
+        if (userDao.getUserByEmail(email).isPresent()) {
             throw new ServiceException(USER_ALREADY_EXISTS);
         }
     }
